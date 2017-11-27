48158eca6747287bdc9012bff9c1b985483a4370
dropboxdssd <segurosmaldonado@outlook.com>
2017-11-27 00:57:12 -0300

    s
    s

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const port = 5000
 
 
 App.get('/', function(req, res) {
-    res.render('subirimagenes.html', {title: 'subir imagenes a dropbox'})
+    window.location.replace("subirimagenes.html");
 })
 
