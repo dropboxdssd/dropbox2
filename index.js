da4758d19eb76ebc127fc43a68818b6e108fb5ed
dropboxdssd <segurosmaldonado@outlook.com>
2017-11-27 00:37:31 -0300

    d

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const port = 5000  
-const options = {  
-  root: __dirname
-}
+
 
 // Definimos funciones para luego usarlas al recibir una petición en el router
 function getHTML(req, res) {  
   res.sendFile('./subirimagenes.html', options, (err) => {
     if (err) throw err;
-    console.log('Sirviendo index.html')
+    console.log('Sirviendo subirimagenes.html')
   })
 }
 
