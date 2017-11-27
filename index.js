b1caee9671bf188472e80f151deae159a04d77b9
dropboxdssd <segurosmaldonado@outlook.com>
2017-11-27 00:42:48 -0300

    fd

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const port = 5000  
 
 
-// Definimos funciones para luego usarlas al recibir una petición en el router
-function getHTML(req, res) {  
-  res.sendFile('./subirimagenes.html', options, (err) => {
-    if (err) throw err;
-    console.log('Sirviendo subirimagenes.html')
-  })
-}
 
-// Definimos las rutas
-App.get('/', getHTML)
+App.get('/', function(req, res) {
+    res.render('subirimagenes.html', {title: 'subir imagenes a dropbox'})
+})
 
